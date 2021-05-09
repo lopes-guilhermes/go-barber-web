01daf738bf385a0e79741ee61cdebe36db846500
Guilherme Lopes <guilhermelopes.silva@hotmail.com>
2021-05-09 18:34:47 -0300

    signIn conectando com a api

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useContext, useRef } from 'react';
 import { FiLock, FiLogIn, FiMail } from 'react-icons/fi'
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import { AuthContext } from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
@@ -12,10 +13,17 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.svg'
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string,
+  password: string
+}
+
 const SignIn: React.FC = () => {
   const formRef= useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const { user, signIn } = useContext(AuthContext);
+
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
       formRef.current?.setErrors({});
       const schema = Yup.object().shape({
@@ -29,11 +37,16 @@ const SignIn: React.FC = () => {
       await schema.validate(data, {
         abortEarly: false //retorna todos erros de uma vez
       });
+      
+      signIn({ 
+        email: data.email, 
+        password: data.password
+      });
     } catch (err) {
       const errors = getValidationErrors(err);
       formRef.current?.setErrors(errors);
     }
-  }, []);
+  }, [signIn]);
   
   return(
     <Container>
